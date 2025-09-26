5ed01bd279ec5287b311b2c9f8bae8f8c0e5614c
Chris Clampitt <christian.clampitt@nyu.edu>
2025-09-26 09:50:32 -0600

    Initial Commit

---
+++ NodesStreamingIntoGrid/NodesStreamingIntoGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodesStreamingIntoGridApp.swift
+//  NodesStreamingIntoGrid
+//
+//  Created by Christian J Clampitt on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NodesStreamingIntoGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
